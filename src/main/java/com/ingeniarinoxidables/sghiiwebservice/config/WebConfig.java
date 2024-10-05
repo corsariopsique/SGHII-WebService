d1e4794ad238bba264d4788fb2195dca0e86402a
corsariopsique <maordo16@gmail.com>
2024-10-05 08:14:47 -0500

    cambio webconfig cors

--- src/main/java/com/ingeniarinoxidables/sghiiwebservice/config/WebConfig.java
+++ src/main/java/com/ingeniarinoxidables/sghiiwebservice/config/WebConfig.java
@@ -1,17 +1,28 @@
 package com.ingeniarinoxidables.sghiiwebservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
     }
 
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
 
 }
